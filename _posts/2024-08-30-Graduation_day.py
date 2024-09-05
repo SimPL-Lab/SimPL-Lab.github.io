7e8769a5ba2516f5d8a9fd3db780dd86f0e78b44
Hoyoung Choi <hy7532799@gmail.com>
2024-09-05 20:45:09 +0900

    write what is updated

---
+++ _posts/2024-08-30-Graduation_day.py
@@ -0,0 +1,31 @@
+---
+
+title:  "Congratulations to Our Recent Graduates!"
+header:
+    teaser: ""
+excerpt: "We are delighted to celebrate the achievements of our recent graduates!"
+author: "Hoyoung"
+categories:
+  - Graduation
+tags:
+  - Ph.D degree
+  - Master degree
+  - Bachelor degree
+last_modified_at: 2024-08-30
+
+---
+<img align="center" width="300" height="300" style="border: 1px solid white" src="/assets/images/Bonggwon_Kang.jpg">
+
+<br>
+We are delighted to celebrate the achievements of our recent graduates!
+
+This year, we proudly congratulate Bonggwon Kang, who has earned his Ph.D. in Industrial Engineering, along with Jungtae Park, Jeam Marne Gutierrez, and Rosi Ana Rahmawati, who have completed their Master’s degrees. We also extend our warmest congratulations to Hoyoung Choi for earning his Bachelor’s degree.
+
+Your dedication, hard work, and passion for research have brought you to this incredible milestone. We are proud of your accomplishments and excited to see the impact you will make in your future careers.
+
+We wish you all the best in your future endeavors and look forward to hearing about your continued successes.
+<span style="font-size: 14px;">
+Congratulations once again!
+
+<br>
+<br>
