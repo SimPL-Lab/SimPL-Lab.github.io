85916ad9649d3feb564ab3b199a9e4b1b92caf4c
Hoyoung Choi <hy7532799@gmail.com>
2024-09-05 22:00:27 +0900

    write what is updated

--- _posts/2024-08-28-SDC_workshop_5.py
+++ _posts/2024-08-28-SDC_workshop_5.py
@@ -1,7 +1,7 @@
 ---
 title:  "Samsung Display & SimPL Lab 5th Joint Workshop (2024.08.28-29) "
 header:
-    teaser: "/assets/images/Workshop(08.28)／JointWorkshop(0828)_Um4.jpg"
+    teaser: "/assets/images/Workshop(08.28)/JointWorkshop(0828)_Um4.jpg"
 excerpt: "2024 Samsung Display & SimPL Lab 5th Joint Workshop was held at PNU on August 28 - 29, 2024."
 author: "hoyoung"
 categories:
@@ -11,8 +11,8 @@ tags:
   - Achievement
 last_modified_at: 2024-08-29
 ---
-<img align="center" width="900" height="900" style="border: 1px solid white" src="/assets/images/Workshop(08.28)／JointWorkshop(0828)_Kim.jpg"> 
-<img align="center" width="900" height="900" style="border: 1px solid white" src="/assets/images/Workshop(08.28)／JointWorkshop(0828)_Um1.jpg"> 
+<img align="center" width="900" height="900" style="border: 1px solid white" src="/assets/images/Workshop(08.28)/JointWorkshop(0828)_Kim.jpg"> 
+<img align="center" width="900" height="900" style="border: 1px solid white" src="/assets/images/Workshop(08.28)/JointWorkshop(0828)_Um1.jpg"> 
 <img align="center" width="900" height="900" style="border: 1px solid white" src="/assets/images/Workshop(08.28)/JointWorkshop(0828)_Lee1.jpg"> 
 <img align="center" width="900" height="900" style="border: 1px solid white" src="/assets/images/Workshop(08.28)/JointWorkshop(0829)_Lee3.jpg""> 
 
