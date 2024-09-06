189707bbb823993041aab2994a237573de17ed9b
Hoyoung Choi <hy7532799@gmail.com>
2024-09-06 12:04:57 +0900

    write what is updated

--- _posts/2024-08-28-SDC_workshop_5.py
+++ _posts/2024-08-28-SDC_workshop_5.py
@@ -1,7 +1,7 @@
 ---
 title:  "Samsung Display & SimPL Lab 5th Joint Workshop (2024.08.28-29) "
 header:
-    teaser: "/assets/images/Workshop(08.28)/JointWorkshop(0828)_Um4.jpg"
+    teaser: "/assets/images/Workshop(08.28)/2024.08.29-SDC_workshop"
 excerpt: "2024 Samsung Display & SimPL Lab 5th Joint Workshop was held at PNU on August 28 - 29, 2024."
 author: "hoyoung"
 categories:
@@ -17,7 +17,7 @@ last_modified_at: 2024-08-29
 <img align="center" width="900" height="900" style="border: 1px solid white" src="/assets/images/Workshop(08.28)/JointWorkshop(0829)_Lee3.jpg""> 
 
 
-Joint Workshop for Prediction of Task Waiting Time Based on STK Log Data was held at PNU on January 18 - 19, 2024.
+Joint Workshop for Prediction of Task Waiting Time Based on STK Log Data was held at PNU on August 28 - 29, 2024.
 
 Samsung Display and SimPL Lab members attended the workshop.
 
