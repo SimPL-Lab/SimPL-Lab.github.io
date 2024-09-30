27c8939cab0c998a513ff85b6d57cbbc05f8a559
Hoyoung Choi <hy7532799@gmail.com>
2024-09-30 19:01:39 +0900

    write what is updated

--- _posts/2024-09-28-Samsung Display_thesis.py
+++ _posts/2024-09-28-Samsung Display_thesis.py
@@ -1,9 +1,9 @@
 ---
 
-title:  "Professor Soon-do Hong's Research Team — Dr. Boseong Kim Receives Silver Award at the 2024 Samsung Display-Hosted Paper Competition"
+title:  "Professor Soondo Hong's Research Team-Ph.D. candidate Bosung Kim Receives Silver Award at the 2024 Samsung Display-Hosted Paper Competition"
 header:
     teaser: "/assets/images/boseong_kim.jpg"
-excerpt: "Ph.D. candidate Boseong Kim from Professor Soon-do Hong's research team won the Silver Award at the 2024 Samsung Display Paper Competition for their work on optimizing job assignments for in-line stockers using genetic algorithms."
+excerpt: "Ph.D. candidate Bosung Kim from Professor Soondo Hong's research team won the Silver Award at the 2024 Samsung Display Paper Competition for their work on optimizing job assignments for in-line stockers using genetic algorithms."
 author: "Hoyoung"
 categories:
   - Samsung Display Thesis
@@ -17,11 +17,11 @@ last_modified_at: 2024-09-28
 <img align="center" width="900" height="900" style="border: 1px solid white" src="/assets/images/boseong_kim.jpg"">
 
 <br>
-Ph.D. candidate Boseong Kim of Professor Soon-do Hong's research team received the Silver Award at the 2024 Samsung Display-hosted paper competition.
+Ph.D. candidate Bosung Kim of Professor Soondo Hong's research team received the Silver Award at the 2024 Samsung Display-hosted paper competition.
 
 The research, titled "Optimizing job assignment for in-line stockers under heavy traffic using genetic algorithm," achieved outstanding results among the award-winning works in mass production or basic technology.
 
-This paper was conducted under the guidance of Professor Soon-do Hong from the Department of Industrial Engineering at Pusan National University, with Ph.D. candidate Boseong Kim participating as the lead author.
+This paper was conducted under the guidance of Professor Soondo Hong from the Department of Industrial Engineering at Pusan National University, with Ph.D. candidate Bosung Kim participating as the lead author.
 
 <span style="font-size: 16px;">
 Congratulations once again!
