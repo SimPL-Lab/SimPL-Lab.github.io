950dc2fc47d481239de928a506a53d1175dcfee0
Hoyoung Choi <hy7532799@gmail.com>
2025-02-24 17:48:51 +0900

    wrrite what is updated

---
+++ _posts/2024-11-29-Paper Pesentation Award.md - 복사본.py
@@ -0,0 +1,23 @@
+---
+
+title:  "The 5th Joint Workshop on the Recent Digital Twin and Production Logistics Research at the University of Washington (2025.01.31-02.06)"
+header:
+    teaser: "/assets/images/4rd Joint Workshop.png"
+excerpt: "We hold in the joint meeting on the recent digital twin research and simulation education at the University of Washington from January 1st to February 6th."
+author: "Hoyong"
+categories:
+  - Paper Presentation Award
+tags:
+  - Paper Presentation Award
+  - Simulation Society
+
+last_modified_at: 2025-02-07
+
+---
+<img align="top" width="500" height="500" style="border: 1px solid white" src="/assets/images/4rd Joint Workshop.png">
+<br>
+<br>
+We hold in the joint meeting on the recent digital twin research and simulation education at the University of Washington from January 1st to February 6th. A couple of meetings on the research topics regarding digital twin constructions were conducted. 
+
+<br>
+<br>
