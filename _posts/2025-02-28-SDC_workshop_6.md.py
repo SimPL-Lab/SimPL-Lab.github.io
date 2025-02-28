605ecf48f34136f33e1efc68f7f110349c72de3a
ryujih <123168733+ryujih@users.noreply.github.com>
2025-02-28 22:15:09 +0900

    write what is updated

---
+++ _posts/2025-02-28-SDC_workshop_6.md.py
@@ -0,0 +1,21 @@
+---
+title:  "Samsung Display & SimPL Lab 6th Joint Workshop (2025.02.19-20) "
+header: 
+    teaser: "/assets/images/2025.02.19-20 6th Workshop (2).jpg" 
+excerpt: "2025 Samsung Display & SimPL Lab 6th Joint Workshop was held at PNU on February 19 - 20, 2025."
+author: "jihyeon"
+categories:
+  - Workshop
+tags:
+  - Workshop
+  - Achievement
+last_modified_at: 2025-02-28
+---
+<img align="center" width="900" height="300" style="border: 1px solid white" src="/assets/images/2025.02.19-20 6th Workshop (1).jpg"> 
+<img align="center" width="900" height="300" style="border: 1px solid white" src="/assets/images/2025.02.19-20 6th Workshop (2).jpg"> 
+
+Joint Workshop for Calibration of Simulation Based on STK Log Data was held at PNU on February 19 - 20, 2025. 
+
+Samsung Display and SimPL Lab members attended the workshop.
+
+Date : February 19-20
