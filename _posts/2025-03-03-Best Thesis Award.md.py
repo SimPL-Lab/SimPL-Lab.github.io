816f94a0efbd556aaf7924e4fd33df58beb6efa6
ryujih <123168733+ryujih@users.noreply.github.com>
2025-03-03 20:18:21 +0900

    write what is updated

---
+++ _posts/2025-03-03-Best Thesis Award.md.py
@@ -0,0 +1,30 @@
+---
+
+title:  "Best Dissertation and Thesis Awards Recipients at Pusan National University Graduation Ceremony"
+header:
+    teaser: "/assets/images/최우수 박사학위 논문상.jpg"
+excerpt: "SimPl lab’s graduates, Bonggwon Kang and Jungtae Park, received Best Dissertation and Thesis Awards."
+author: "Jihyeon"
+categories:
+  - Award
+tags:
+  - Best Dissertation Award
+  - Best Thesis Award
+last_modified_at: 2025-03-03
+
+---
+<img align="center" width="400" height="560" style="border: 1px solid white" src="/assets/images/최우수 박사학위 논문상.jpg"> 
+<img align="center" width="400" height="560" style="border: 1px solid white" src="/assets/images/최우수 석사학위 논문상.jpg">
+
+<br>
+
+We are proud to announce that SimPl lab’s graduates, Bonggwon Kang and Jungtae Park, have been recognized for their outstanding research at the Pusan National University graduation ceremony. 
+
+Bonggwon Kang received the Best PhD Dissertation Award, while Jungtae Park was honored with the Best Master’s Thesis Award. 
+
+Congratulations to both on this exceptional achievement!
+
+
+<br>
+<br>
+
